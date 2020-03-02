1098be3aac55e12cc17269b9341d54724f025e6c
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:20:14 +0200

    Add SpringBoot main class

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/SpringBootOpticsWebAppApplication.java
@@ -0,0 +1,20 @@
+package com.bubnii.springBoot_opticsWebApp;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootOpticsWebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootOpticsWebAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
