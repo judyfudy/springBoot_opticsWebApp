4166c7b5847bfefb70c5f8f03d3efea507ee8a09
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:31:24 +0200

    add WorkerRepository interface

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/repository/interfaces/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.bubnii.springBoot_opticsWebApp.repository.interfaces;
+
+import com.bubnii.springBoot_opticsWebApp.entity.Worker;
+
+public interface WorkerRepository extends Repository<Worker> {
+
+}
