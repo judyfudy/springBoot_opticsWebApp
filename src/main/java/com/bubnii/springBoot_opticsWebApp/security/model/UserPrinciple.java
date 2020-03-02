58fc94b204ef7f0fdbae755988e40231b1917efc
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:47:31 +0200

    add UserPrinciple security model

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/security/model/UserPrinciple.java
@@ -0,0 +1,54 @@
+package com.bubnii.springBoot_opticsWebApp.security.model;
+
+import com.bubnii.springBoot_opticsWebApp.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPrinciple implements UserDetails {
+    private Integer id;
+    private String username;
+    private String email;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() { return null; }
+
+    public static UserPrinciple build(final User user) {
+        return new UserPrinciple(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail());
+    }
+}
