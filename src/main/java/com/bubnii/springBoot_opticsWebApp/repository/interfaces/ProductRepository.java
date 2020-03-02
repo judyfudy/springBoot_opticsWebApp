816d224633132cf1de452ef120f50e5cbe3521e6
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:31:38 +0200

    add ProductRepository interface

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/repository/interfaces/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.bubnii.springBoot_opticsWebApp.repository.interfaces;
+
+import com.bubnii.springBoot_opticsWebApp.entity.Product;
+import com.bubnii.springBoot_opticsWebApp.entity.ProductType;
+
+import java.util.List;
+
+public interface ProductRepository extends Repository<Product> {
+    void addProductToCart(final int personId, final int productId);
+
+    List<Product> getProductsByType(final int typeId);
+
+    List<ProductType> getAllTypesOfProducts();
+
+    List<Product> getAllProductsInCart(final int userId);
+
+    void deleteFromCart(final int productId, final int userId);
+
+    void clearCart(final int userId);
+}
