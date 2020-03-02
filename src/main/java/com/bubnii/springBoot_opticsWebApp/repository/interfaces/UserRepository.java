8f98814c9a3a1cbfa4c8575390bda351a6ed8306
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:31:08 +0200

    add UserRepository interface

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/repository/interfaces/UserRepository.java
@@ -0,0 +1,7 @@
+package com.bubnii.springBoot_opticsWebApp.repository.interfaces;
+
+import com.bubnii.springBoot_opticsWebApp.entity.User;
+
+public interface UserRepository extends Repository<User> {
+    User getPersonByCredentials(final String username);
+}
