e764e002c444cc4bf4638553202f039f49b52a1a
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:55:17 +0200

    add ProductTypeController

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/controller/ProductTypeController.java
@@ -0,0 +1,27 @@
+package com.bubnii.springBoot_opticsWebApp.controller;
+
+
+import com.bubnii.springBoot_opticsWebApp.dto.ProductTypeDTO;
+import com.bubnii.springBoot_opticsWebApp.service.interfaces.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productType")
+public class ProductTypeController {
+
+    private final ProductService productService;
+
+    public ProductTypeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/all")
+    public List<ProductTypeDTO> productTypeList() {
+        return productService.getAllTypesOfProducts();
+    }
+}
