a7f7bc1035d36e17df1d71e807bc891795a333a3
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:54:49 +0200

    add WorkerController

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/controller/WorkerController.java
@@ -0,0 +1,25 @@
+package com.bubnii.springBoot_opticsWebApp.controller;
+
+import com.bubnii.springBoot_opticsWebApp.dto.WorkerDTO;
+import com.bubnii.springBoot_opticsWebApp.service.interfaces.WorkerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/worker")
+public class WorkerController {
+
+    private final WorkerService workerService;
+
+    public WorkerController(final WorkerService workerService) {
+        this.workerService = workerService;
+    }
+
+    @GetMapping("/all")
+    public List<WorkerDTO> getAllWorkers() {
+        return workerService.getAll();
+    }
+}
