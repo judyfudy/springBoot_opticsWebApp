7b1bb85509b77967b2b8ec4a8a5f9f730307cfa4
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-03-02 14:45:50 +0200

    add MessageResponse security dto class

---
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/security/dto/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.bubnii.springBoot_opticsWebApp.security.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
