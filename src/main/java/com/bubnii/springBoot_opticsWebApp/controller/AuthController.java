a0bdb32438dec3ae8bc23e5aa9bb8973124144a3
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-06-21 21:35:17 +0300

    add method to login with Facebook

--- src/main/java/com/bubnii/springBoot_opticsWebApp/controller/AuthController.java
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.bubnii.springBoot_opticsWebApp.controller;
 
+import com.bubnii.springBoot_opticsWebApp.dto.UserDTO;
 import com.bubnii.springBoot_opticsWebApp.entity.User;
+import com.bubnii.springBoot_opticsWebApp.security.dto.FacebookUserData;
 import com.bubnii.springBoot_opticsWebApp.security.dto.JwtResponse;
 import com.bubnii.springBoot_opticsWebApp.security.dto.LoginForm;
 import com.bubnii.springBoot_opticsWebApp.security.dto.MessageResponse;
 import com.bubnii.springBoot_opticsWebApp.security.jwt.JwtProvider;
+import com.bubnii.springBoot_opticsWebApp.service.interfaces.FacebookService;
 import com.bubnii.springBoot_opticsWebApp.service.interfaces.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,12 +15,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -26,11 +27,14 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
     private final JwtProvider jwtProvider;
+    private final FacebookService facebookService;
 
-    public AuthController(final AuthenticationManager authenticationManager, final UserService userService, final JwtProvider jwtProvider) {
+    public AuthController(final AuthenticationManager authenticationManager, final UserService userService,
+                          final JwtProvider jwtProvider, final FacebookService facebookService) {
         this.authenticationManager = authenticationManager;
         this.userService = userService;
         this.jwtProvider = jwtProvider;
+        this.facebookService = facebookService;
     }
 
     @PostMapping("/authenticate")
@@ -46,13 +50,31 @@ public class AuthController {
         return ResponseEntity.ok(new MessageResponse("User has been successfully registered: " + user.getEmail()));
     }
 
-    private ResponseEntity<JwtResponse> authenticate(final LoginForm loginRequest) {
+    @PostMapping("/signinFacebook")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<JwtResponse> authenticateFacebookUser(@Valid @RequestBody FacebookUserData userData) {
+        LoginForm form = facebookService.authenticateUser(userData);
+        return authenticate(form);
+    }
 
+    @GetMapping("/signup")
+    @PreAuthorize("permitAll()")
+    public boolean checkUniqueUsername(@RequestParam("name") final String username) {
+        List<UserDTO> usersList = userService.getAll();
+
+        for (UserDTO iterator : usersList) {
+            if (iterator.getUsername().contains(username)) {
+                return true; // if true - user exists
+            }
+        }
+        return false; // if false - user doesn't exists
+    }
+
+    private ResponseEntity<JwtResponse> authenticate(final LoginForm loginRequest) {
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                ));
+                        loginRequest.getPassword()));
 
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
