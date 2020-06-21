50f55f4a8e53bfdefcdbc616831ffcfb3366693f
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-06-21 21:31:09 +0300

    add new method: deleteProduct(), now Admin can delete product in catalog

--- src/main/java/com/bubnii/springBoot_opticsWebApp/controller/ProductController.java
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.bubnii.springBoot_opticsWebApp.controller;
 
 import com.bubnii.springBoot_opticsWebApp.dto.ProductDTO;
+import com.bubnii.springBoot_opticsWebApp.security.dto.MessageResponse;
 import com.bubnii.springBoot_opticsWebApp.service.interfaces.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +22,11 @@ public class ProductController {
     @GetMapping("/all/{typeId}")
     public List<ProductDTO> getProductsByType(@PathVariable final int typeId) {
         return productService.getProductsByType(typeId);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<MessageResponse> deleteProduct(@RequestParam("productId") final int productId) {
+        productService.delete(productId);
+        return ResponseEntity.ok(new MessageResponse("Product " + productId + "successfully deleted"));
     }
 }
