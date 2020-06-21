ca14849318f35dbedcf28c053feeeac5a658f474
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-06-21 21:28:56 +0300

    add UserType field

--- src/main/java/com/bubnii/springBoot_opticsWebApp/dto/UserDTO.java
+++ src/main/java/com/bubnii/springBoot_opticsWebApp/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.bubnii.springBoot_opticsWebApp.dto;
 
+import com.bubnii.springBoot_opticsWebApp.enums.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,11 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(exclude = {"firstName", "lastName", "email"})
+@EqualsAndHashCode(exclude = {"firstName", "lastName", "email", "userType"})
 public class UserDTO {
     private int id;
     private String username;
     private String firstName;
     private String lastName;
     private String email;
+    private UserType userType;
 }
